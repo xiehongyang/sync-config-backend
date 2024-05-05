8f0473991e61b17237ef1590c0be359ef0530560
steve <xhysteve@gmail.com>
2024-05-06 00:49:51 +1000

    fixing import hbs bug

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from 'path';
+var hbs = require('hbs');
 import { ResponseInterceptor } from "src/response/response.interceptor";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -11,8 +12,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.set('view engine', 'html');
-  app.engine('html', require('hbs').__express);
+  app.setViewEngine('hbs');
   app.enableCors();
 
   await app.listen(3000);
